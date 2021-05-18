3757a1a15373c2c9e88a039067b4521fa037366f
a19189114 <SAVolegov@sberbank.ru>
2021-05-18 09:43:19 +0300

    code cleaned

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -1,15 +1,8 @@
 package controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
-import framework.annotations.RequestMapping;
-import model.entities.User;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 public interface AccountController {
-
 
       void handleCardRequest(HttpExchange exchange) throws Exception;
 }
--- src/main/java/framework/ApplicationContext.java
+++ src/main/java/framework/ApplicationContext.java
@@ -1,15 +1,6 @@
 package framework;
 
-import framework.annotations.Controller;
-
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Objects;
 
 public interface ApplicationContext {
     List<Class<?>> getControllers();
